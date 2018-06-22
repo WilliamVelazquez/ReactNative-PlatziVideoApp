01eddfdc15b0e3adea7c694373dec1bd931b19d9
William Velazquez <william.velazquez.a@gmail.com>
2018-06-22 01:24:15 -0500

    Header Component
    Adding assets
    Creating Header Component with safe area view for iPhone X Notch
    Adding Header styles

--- platziVideoApp/App.js
+++ platziVideoApp/App.js
@@ -4,12 +4,14 @@ import {
 } from 'react-native';
 
 import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
+        <Header />
         <Text>header</Text>
         <Text>buscador</Text>
         <Text>categorías</Text>
