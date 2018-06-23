192980426cbd4f094c107921cb842c2d4b70c8e1
William Velazquez <william.velazquez.a@gmail.com>
2018-06-23 12:58:02 -0500

    ListEmptyComponent and ItemSeparatorComponent

--- platziVideoApp/src/videos/containers/suggestion-list.js
+++ platziVideoApp/src/videos/containers/suggestion-list.js
@@ -4,24 +4,30 @@ import {
   Text
 } from 'react-native';
 import Layout from '../components/suggestion-list-layout';
+import Empty from '../components/empty';
+import Separator from '../components/vertical-separator';
 
 class SuggestionList extends Component {
+  renderEmpty = () => <Empty text="No hay sugerencias :(" />
+  itemSeparator = () => <Separator />
 	render() {
 		const list = [
-			{
-				title: 'título',
-				key: '1'
-			},
-			{
-				title: 'otro título',
-				key: '2'
-			}
+      {
+        title: 'Avengers',
+        key: '1'        
+      },
+      {
+        title: 'Pokemon',
+        key: '2'        
+      }
 		];
 		return(
       <Layout
         title="Recomendado para ti">
         <FlatList 
           data={list}
+          ListEmptyComponent={this.renderEmpty}
+          ItemSeparatorComponent={this.itemSeparator}
           renderItem={({ item }) => <Text>{item.title}</Text>}
         />
       </Layout>
