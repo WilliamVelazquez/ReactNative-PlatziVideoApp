c96eb064e97c3fcd419e3a739160f0e4652efc10
William Velazquez <william.velazquez.a@gmail.com>
2018-06-23 18:03:37 -0500

    HorizontalSeparator Component
    Adding HorizontalSeparator Component
    Moving VerticalSeparator Component
    Updating VerticalSeparator Component
    Updating CategoryList Separator

--- platziVideoApp/src/sections/components/vertical-separator.js
+++ platziVideoApp/src/sections/components/vertical-separator.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {
   View,
-  Text,
   StyleSheet
 } from 'react-native';
 
 function VerticalSeparator(props){
   return(
-    <View style={[
-      styles.separator,
-      {
-        borderTopColor: (props.color) ? props.color : '#eaeaea'
-      }
-    ]}>
-    </View>
+    <View 
+      style={[
+        styles.separator,
+        {borderTopColor: (props.color) ? props.color : '#eaeaea'}
+      ]}
+    />
   )
 }
 
