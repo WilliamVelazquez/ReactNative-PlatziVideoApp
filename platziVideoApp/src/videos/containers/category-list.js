2cee6d3c8e0be8adfa727b8a20cedde4c3b6a947
William Velazquez <william.velazquez.a@gmail.com>
2018-06-24 00:09:42 -0500

    Category Component
    Adding Category Component
    Adding Styles to Category Component

--- platziVideoApp/src/videos/containers/category-list.js
+++ platziVideoApp/src/videos/containers/category-list.js
@@ -6,7 +6,7 @@ import {
 import Layout from '../components/category-list-layout';
 import Empty from '../components/empty';
 import Separator from '../../sections/components/horizontal-separator';
-import Suggestion from '../components/suggestion';
+import Category from '../components/category';
 
 class CategoryList extends Component{
   keyExtractor = (item) => item.id.toString();
@@ -14,7 +14,7 @@ class CategoryList extends Component{
   itemSeparator = () => <Separator />;
   renderItem = ({item}) => {
     return(
-      <Suggestion {...item}/>
+      <Category {...item}/>
     )
   };
   render(){
